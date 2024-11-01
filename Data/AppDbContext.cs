ef7342da26aa07a3765834b1fe6d3d4a10720f81
Erick Lemos <ericklemos.dev@gmail.com>
2024-11-01 18:22:45 -0300

    String de conexão criada

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using FazendaUrbanaApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FazendaUrbanaApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+
+    }
+}
---
+++ Models/ResponseModel.cs
@@ -0,0 +1,9 @@
+﻿namespace FazendaUrbanaApi.Models
+{
+    public class ResponseModel<T>
+    {
+        public T? Dados { get; set; }
+        public string Mensagem { get; set; } = string.Empty;
+        public bool Status {  get; set; } = true;
+    }
+}
